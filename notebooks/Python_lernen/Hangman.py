4ac226354d0ae8ac7d41499a1037714bf36f0fde
PhilippR84 <Philipp@Rambacher.net>
2024-01-05 12:28:26 +0100

    Hangmann_Game_File_Open

--- notebooks/Python_lernen/Hangman.py
+++ notebooks/Python_lernen/Hangman.py
@@ -1,66 +1,14 @@
-"""
-
-chatgpt: 
-
 import random
+Words = []
+with open("Hangman_words.txt","r",encoding="UTF8") as datei:
+    int_Rows_datei = 0
+    for x in datei:
+        int_Rows_datei = int_Rows_datei + 1
+        line_Words = x
+        Words.append(line_Words)
 
-def choose_word():
-    words = ["python", "java", "ruby", "javascript", "html", "css", "php"]
-    return random.choice(words)
-
-def hangman():
-    word_to_guess = choose_word()
-    guessed_letters = set()
-    attempts = 6
-
-    while attempts > 0:
-        display_word = ""
-        for letter in word_to_guess:
-            if letter in guessed_letters:
-                display_word += letter
-            else:
-                display_word += "_"
-
-        print(display_word)
-
-        if display_word == word_to_guess:
-            print("Congratulations! You guessed the word.")
-            break
-
-        guess = input("Enter a letter: ").lower()
-
-        if guess in guessed_letters:
-            print("You already guessed that letter. Try again.")
-            continue
-
-        guessed_letters.add(guess)
-
-        if guess not in word_to_guess:
-            attempts -= 1
-            print(f"Incorrect! You have {attempts} attempts left.")
-
-        if attempts == 0:
-            print(f"Sorry, you ran out of attempts. The word was {word_to_guess}.")
-
-# Uncomment the line below to play the game
-# hangman()
-"""
-
-# Zufälliges Wort auswählen
-# Ausgabe mit "_" wie viele Zeichen das Wort hat
-# Eingabe welchen Buchstabe man wählt
-# Ausgabe ob der Buchstabe im Wort vorhanden ist 
-# Wenn Ja: Buchstabe angben
-# Wenn nein: counter hochzählen
-
-"""
-with open('\notebooks\Python_lernen\Hangman_words.txt', 'r') as file:
-    # Lies alle Zeilen und speichere sie in einer Liste
-    list = file.readlines()
-    print(list)
-"""
-
-datei = open('C:/Users/Phili/OneDrive/DHBW/VSCode/Philipps_Repo/notebooks/Python_lernen/Hangman_words.txt','r')
-print(datei)
-
+Random_int = (random.randint(0,int_Rows_datei))
+Random_int = Random_int  - 1
+print(Words[Random_int])
+print(Words)
 
